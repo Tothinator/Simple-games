3664e320a0414e4bf518f3badde4fb26d5597ec5
David Toth <dtoth2016@gmail.com>
2019-03-04 14:25:32 -0600

    looks like I'm done with Psychic game, need to find word bank for word guess and make a few changes to make psychic code work with word guess

---
+++ assets/js/main.js
@@ -0,0 +1,92 @@
+var playstyle = 0; // 0 is default keyboard playstyle, 1 is button playstyle
+var guessedLetters = [];
+var guesses = 10;
+var answer = "";
+letters = "abcdefghijklmnopqrstuvwxyz";
+
+document.addEventListener("DOMContentLoaded", function() {
+    setDefaults();
+    playstyle = 0;
+    document.getElementById("keyboard-instructions").style.display = "inline";
+    document.getElementById("button-instructions").style.display = "none";
+    document.getElementById("btn-container").style.display = "none";
+    document.getElementById("playstyle-btn").textContent = "Play with buttons!";
+});
+
+function setDefaults() {
+    guesses = 10;
+    answer = getRandomLetter();
+    console.log("This is the answer! " + answer);
+    document.getElementById("answer").textContent = answer.toUpperCase();
+    document.getElementById("guessNumber").textContent = guesses;
+    document.getElementById("answer").style.visibility = "hidden";
+    document.getElementById("congrats-message").style.display = "none";
+    var guessDivs = document.getElementById("guessed-letters");
+    while (guessDivs.firstChild){
+        guessDivs.removeChild(guessDivs.firstChild);
+    }
+    for (var i = 0; i < guessedLetters.length; i++){
+        document.getElementById(guessedLetters[i]).classList.remove("btn-disabled");
+    }
+    guessedLetters = [];
+}
+
+function changePlaystyle() {
+    if (playstyle === 0) {
+        document.getElementById("keyboard-instructions").style.display = "none";
+        document.getElementById("button-instructions").style.display = "inline";
+        document.getElementById("btn-container").style.display = "block";
+        document.getElementById("guessed-letters-title").style.display = "none";
+        document.getElementById("guessed-letters").style.display = "none";
+        document.getElementById("playstyle-btn").textContent = "Play with Keyboard!";
+        playstyle++;
+    } else {
+        document.getElementById("keyboard-instructions").style.display = "inline";
+        document.getElementById("button-instructions").style.display = "none";
+        document.getElementById("btn-container").style.display = "none";
+        document.getElementById("guessed-letters-title").style.display = "block";
+        document.getElementById("guessed-letters").style.display = "flex";
+        document.getElementById("playstyle-btn").textContent = "Play with buttons!";
+        playstyle--;
+    }
+    console.log(playstyle);
+}
+
+function getRandomLetter() {
+    return letters.charAt(Math.floor(Math.random() * letters.length));
+}
+
+function letterGuess(letter) {
+    if (letter === answer && guesses !== 0) {
+        document.getElementById("answer").style.visibility = "visible";
+        document.getElementById("win-or-lose").textContent = "Nice!"
+        document.getElementById("congrats-message").style.display = "block";
+    } else if (guesses !== 0) {
+        addGuess(letter);
+        document.getElementById("guessNumber").textContent = guesses;
+    }
+    if (guesses === 0) {
+        document.getElementById("win-or-lose").textContent = "Sorry!"
+        document.getElementById("congrats-message").style.display = "block";
+    } 
+}
+
+function addGuess(letter) {
+    if (letters.includes(letter) && guessedLetters.indexOf(letter) < 0) {
+        guesses--;
+        guessedLetters.push(letter);
+        var node = document.createElement("div");
+        node.appendChild(document.createTextNode(letter.toUpperCase()));
+        node.classList.add("col-1");
+        node.classList.add("text-center");
+        node.classList.add("large-letter");
+        document.getElementById("guessed-letters").appendChild(node);
+        document.getElementById(letter).classList.add("btn-disabled");
+    }
+}
+
+document.onkeyup = function(event) {
+    if (playstyle === 0) {
+        letterGuess(event.key);
+    }
+}
